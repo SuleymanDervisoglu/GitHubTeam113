b2ff99fd716ef3ed6c734d9aa03ea8a52d6cfaa9
SuleymanDervisoglu <sderwise@gmail.com>
2023-06-08 20:31:12 +0200

    ilk degisiklik

--- src/personelUse/GitHub1.java
+++ src/personelUse/GitHub1.java
@@ -3,5 +3,7 @@ package personelUse;
 public class GitHub1 {
     public static void main(String[] args) {
         System.out.println("Hello gitHub");
+
+        System.out.println("Thats it");
     }
 }
